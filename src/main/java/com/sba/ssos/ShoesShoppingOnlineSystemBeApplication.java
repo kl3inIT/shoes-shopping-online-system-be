d3fd9cd10e159f21b4b36bd067c322ad1c6dd159
kl3inIT <dathip04@gmail.com>
2026-01-16 13:48:17 +0700

    init project

---
+++ src/main/java/com/sba/ssos/ShoesShoppingOnlineSystemBeApplication.java
@@ -0,0 +1,12 @@
+package com.sba.ssos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesShoppingOnlineSystemBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoesShoppingOnlineSystemBeApplication.class, args);
+  }
+}
---
+++ src/test/java/com/sba/ssos/ShoesShoppingOnlineSystemBeApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sba.ssos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesShoppingOnlineSystemBeApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
