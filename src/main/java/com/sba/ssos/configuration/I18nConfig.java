837b0116b1a6de216e388148b7f12e4ff6661306
kl3inIT <dathip04@gmail.com>
2026-01-21 14:55:31 +0700

    feat(i18n): implement internationalization support with message source and locale resolver, and update controller to return localized messages

---
+++ src/main/java/com/sba/ssos/configuration/I18nConfig.java
@@ -0,0 +1,53 @@
+package com.sba.ssos.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
+@Configuration
+public class I18nConfig {
+
+    /**
+     * Configures the message source for internationalization.
+     * <p>
+     * This method sets up a ResourceBundleMessageSource to load messages from the "messages" properties file.
+     *
+     * @return a MessageSource object configured for internationalization
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setDefaultLocale(Locale.ENGLISH);
+        messageSource.setCacheSeconds(3600);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        return resolver;
+    }
+
+
+    /**
+     * Configures the LocalValidatorFactoryBean for validation.
+     * <p>
+     * This method sets up a LocalValidatorFactoryBean to use the message source for validation messages.
+     *
+     * @return a LocalValidatorFactoryBean object configured for validation
+     */
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean factory = new LocalValidatorFactoryBean();
+        factory.setValidationMessageSource(messageSource());
+        return factory;
+    }
+}
